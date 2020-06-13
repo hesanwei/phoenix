14c36f87297d81c05e5d4c30f06e6b7b02360e4a
hesanwei <hesanwei.bj@acewill.cn>
2020-06-13 22:40:14 +0800

    mine  一级页面部分UI

--- app/src/main/java/com/fhhy/phoenix/base/BaseApplication.kt
+++ app/src/main/java/com/fhhy/phoenix/base/BaseApplication.kt
@@ -1,35 +1,40 @@
 package com.fhhy.phoenix.base
 
-import android.app.Application
 import android.content.Context
+import androidx.multidex.MultiDexApplication
 import com.fhhy.phoenix.R
 import com.scwang.smart.refresh.footer.ClassicsFooter
 import com.scwang.smart.refresh.header.ClassicsHeader
 import com.scwang.smart.refresh.layout.SmartRefreshLayout
-import com.scwang.smart.refresh.layout.api.RefreshHeader
-import com.scwang.smart.refresh.layout.api.RefreshLayout
-import com.scwang.smart.refresh.layout.listener.DefaultRefreshHeaderCreator
 
 // Created by admin on 2020/6/7.
-object BaseApplication : Application() {
+class BaseApplication : MultiDexApplication() {
+
+    init {
+        initSmartRefreshLayoutHeader()
+    }
+
     override fun onCreate() {
         super.onCreate()
-        initSmartRefreshLayoutHeader()
+        sContext = applicationContext
     }
 
     private fun initSmartRefreshLayoutHeader() {
         SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
-            layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
-            ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+            layout.setPrimaryColorsId(R.color.white, android.R.color.black);//全局设置主题颜色
+            ClassicsHeader(context)//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
         }
 
         SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
-            layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+            layout.setPrimaryColorsId(R.color.white, android.R.color.black);//全局设置主题颜色
             ClassicsFooter(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
         }
     }
 
-    fun getAppContext(): Context {
-        return applicationContext
+    companion object {
+        private var sContext: Context? = null
+        fun getAppContext(): Context {
+            return sContext!!
+        }
     }
 }
--- app/src/main/java/com/fhhy/phoenix/mine/fragment/MineFragment.kt
+++ app/src/main/java/com/fhhy/phoenix/mine/fragment/MineFragment.kt
@@ -5,6 +5,8 @@ import com.fhhy.phoenix.R
 import com.fhhy.phoenix.base.BaseMvpFragment
 import com.fhhy.phoenix.mine.MineContract
 import com.fhhy.phoenix.mine.presenter.MinePresenter
+import com.jaeger.library.StatusBarUtil
+import kotlinx.android.synthetic.main.fragment_mine.*
 
 // Created by admin on 2020/6/7.
 class MineFragment : BaseMvpFragment<MineContract.View, MineContract.Presenter>() {
@@ -15,7 +17,8 @@ class MineFragment : BaseMvpFragment<MineContract.View, MineContract.Presenter>(
 
     override fun initView(view: View) {
         super.initView(view)
-
+        StatusBarUtil.setTransparentForImageView(activity, smartRefreshLayout)
+        StatusBarUtil.setLightMode(activity)
     }
 
     override fun lazyLoad() {
