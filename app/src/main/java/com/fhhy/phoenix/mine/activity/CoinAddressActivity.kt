af59c005301c0a92ac426f1e6743019becf0e797
hesanwei <hesanwei.bj@acewill.cn>
2020-06-21 16:34:06 +0800

    添加提币地址UI done

--- app/src/main/java/com/fhhy/phoenix/mine/activity/CoinAddressActivity.kt
+++ app/src/main/java/com/fhhy/phoenix/mine/activity/CoinAddressActivity.kt
@@ -1,5 +1,7 @@
 package com.fhhy.phoenix.mine.activity
 
+import android.content.Intent
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fhhy.phoenix.R
 import com.fhhy.phoenix.base.BaseMvpActivity
@@ -8,11 +10,14 @@ import com.fhhy.phoenix.mine.adapter.CoinAddressAdapter
 import com.fhhy.phoenix.mine.contract.CoinAddressContract
 import com.fhhy.phoenix.mine.presenter.CoinAddressPresenter
 import kotlinx.android.synthetic.main.activity_coin_address.*
+import setViewClickListener
 
 // Created by admin on 2020/6/21.
 class CoinAddressActivity :
     BaseMvpActivity<CoinAddressContract.View, CoinAddressContract.Presenter>(),
-    CoinAddressContract.View {
+    CoinAddressContract.View, View.OnClickListener {
+
+    private var fundsName: String? = ""
 
     private val coinAddressAdapter by lazy {
         CoinAddressAdapter()
@@ -24,9 +29,10 @@ class CoinAddressActivity :
 
     override fun initView() {
         super.initView()
-        val fundsName = intent.getStringExtra(FUNDS_NAME)
-        tvTitle.text = String.format(resources.getString(R.string.coin_withdraw_address),fundsName)
+        fundsName = intent.getStringExtra(FUNDS_NAME)
+        tvTitle.text = String.format(resources.getString(R.string.coin_withdraw_address), fundsName)
         initRecyclerView()
+        setViewClickListener(this, btnBack, tvAddNewAddress)
     }
 
     private fun initRecyclerView() {
@@ -35,6 +41,18 @@ class CoinAddressActivity :
             layoutManager = LinearLayoutManager(this@CoinAddressActivity)
             coinAddressAdapter.data = mutableListOf("", "", "")
             adapter = coinAddressAdapter
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnBack -> finish()
+
+            R.id.tvAddNewAddress -> {
+                val intent = Intent(this, AddNewAddressActivity::class.java)
+                intent.putExtra(FUNDS_NAME, fundsName)
+                startActivity(intent)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/fhhy/phoenix/mine/model/AddNewAddressModel.kt
@@ -0,0 +1,9 @@
+package com.fhhy.phoenix.mine.model
+
+import com.fhhy.phoenix.base.BaseModel
+import com.fhhy.phoenix.mine.contract.AddNewAddressContract
+
+// Created by admin on 2020/6/21.
+class AddNewAddressModel:BaseModel(),AddNewAddressContract.Model {
+
+}
